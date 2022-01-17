41e4b7b16a02d24897322bf59402462aaec29976
Rameez Ali <rameez.hashone@gmail.com>
2022-01-17 16:37:49 +0500

    status code updated

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -56,7 +56,7 @@ class AuthenticationController extends Controller
             return response()->json([$success, 'User logged In successfully.'], 200);
         } 
         else{ 
-            dd(response()->json(['statusCode'=>'419','message' =>'Unauthorized'], 419));
+            return response()->json(['statusCode'=>'419', 'message' =>'Unauthorized'], 419);
         } 
     }
 }
