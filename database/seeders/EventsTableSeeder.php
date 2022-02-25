12a9884fa600932e889d608b378f615011e1f46f
Rameez Ali <rameez.hashone@gmail.com>
2022-02-25 13:04:25 +0500

    status code updated

--- database/seeders/EventsTableSeeder.php
+++ database/seeders/EventsTableSeeder.php
@@ -19,36 +19,36 @@ class eventsTableSeeder extends Seeder
          * Events
          *
          */
-        $events = [
-            [
-            'event_name'        => 'event1',
-            'details'           => 'event1',
-            'start_date'        => Carbon::now(),
-            'end_date'          => Carbon::now()->addDay(2),
-            'meal_type'         => 'snaks',
-            'status'            => 1,
-            ],
-            [
-            'event_name'        => 'event2',
-            'details'           => 'event2',
-            'start_date'        => Carbon::now(),
-            'end_date'          => Carbon::now()->addDay(2),
-            'meal_type'         => 'snaks',
-            'status'            => 0,
-            ],
-            [
-            'event_name'        => 'event3',
-            'details'           => 'event3',
-            'start_date'        => Carbon::now(),
-            'end_date'          => Carbon::now()->addDay(2),
-            'meal_type'         => 'snaks',
-            'status'            => 1,
-            ],
-        ];
+        // $events = [
+        //     [
+        //     'event_name'        => 'event1',
+        //     'details'           => 'event1',
+        //     'start_date'        => Carbon::now(),
+        //     'end_date'          => Carbon::now()->addDay(2),
+        //     'meal_type'         => 'snaks',
+        //     'status'            => 1,
+        //     ],
+        //     [
+        //     'event_name'        => 'event2',
+        //     'details'           => 'event2',
+        //     'start_date'        => Carbon::now(),
+        //     'end_date'          => Carbon::now()->addDay(2),
+        //     'meal_type'         => 'snaks',
+        //     'status'            => 0,
+        //     ],
+        //     [
+        //     'event_name'        => 'event3',
+        //     'details'           => 'event3',
+        //     'start_date'        => Carbon::now(),
+        //     'end_date'          => Carbon::now()->addDay(2),
+        //     'meal_type'         => 'snaks',
+        //     'status'            => 1,
+        //     ],
+        // ];
     
-        foreach($events as $event) {
-            Event::create($event);
-        }
-        echo "\e[32mSeeding:\e[0m Event - complete\r\n";
+        // foreach($events as $event) {
+        //     Event::create($event);
+        // }
+        // echo "\e[32mSeeding:\e[0m Event - complete\r\n";
     }
 }
