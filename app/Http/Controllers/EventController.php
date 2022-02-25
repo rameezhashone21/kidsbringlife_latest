ca48bec685902fd4b4b20b26dffbded0c69de1d7
Rameez Ali <rameez.hashone@gmail.com>
2022-02-25 15:12:05 +0500

    event assigned to user

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -147,7 +147,7 @@ class EventController extends Controller
             ->first();
 
 
-        if ($event > 0) {
+        if ($event) {
             return response(["result" => $event, 'status' => '200', 'message' => 'My Event'], 200);
         } else {
             return response(['status' => '0', 'message' => 'No Data Found'], 200);
